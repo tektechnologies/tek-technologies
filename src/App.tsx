23c6be3f8e8f3b5881f051d13d777901e76342db
tektechnologies <info@tektechnologies.com>
2025-10-22 16:00:28 -0500

    add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./theme/index.css";
 import Header from "./components/nav/header";
+import Footer from "./components/footer/footer";
 import "./theme/index.css";
 
 function App() {
@@ -83,55 +84,7 @@ function App() {
           </a>
         </section>
 
-        <footer>
-          <div className="footer-content">
-            <div className="footer-section">
-              <h4>Products</h4>
-              <ul>
-                <li>
-                  <a href="#">Video Systems</a>
-                </li>
-                <li>
-                  <a href="#">Internet of Things Devices</a>
-                </li>
-                <li>
-                  <a href="#">Internet Services in select markets</a>
-                </li>
-              </ul>
-            </div>
-            <div className="footer-section">
-              <h4>Resources</h4>
-              <ul>
-                <li>
-                  <a href="#">Documentation</a>
-                </li>
-                <li>
-                  <a href="#">Case Studies</a>
-                </li>
-                <li>
-                  <a href="#">White Papers</a>
-                </li>
-              </ul>
-            </div>
-            <div className="footer-section">
-              <h4>Company</h4>
-              <ul>
-                <li>
-                  <a href="#">About Us</a>
-                </li>
-                <li>
-                  <a href="#">Careers</a>
-                </li>
-                <li>
-                  <a href="#">Contact</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="footer-bottom">
-            <p>&copy; 2025 TEK Technologies IoT. All rights reserved.</p>
-          </div>
-        </footer>
+        <Footer />
       </body>
     </>
   );
