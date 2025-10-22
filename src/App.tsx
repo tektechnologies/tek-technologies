41e17104b05feba708bcd43c6be737b10b5da264
tektechnologies <info@tektechnologies.com>
2025-10-22 16:49:11 -0500

    casing error fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import "./theme/index.css";
-import Header from "./components/nav/Header";
-import Main from "./components/main/MainSection";
-import Footer from "./components/footer/Footer";
-import "./theme/index.css";
+import Header from "./components/nav/Header.tsx";
+import Main from "./components/main/MainSection.tsx";
+import Footer from "./components/footer/Footer.tsx";
 
 function App() {
   return (
     <>
-        <Header />
-        <Main />
-        <Footer />
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
